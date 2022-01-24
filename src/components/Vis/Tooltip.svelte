d728434438b4db19f66cedf0eef67daf5870de5e
Aucher Serr <aucher.serr@gmail.com>
2022-01-24 13:28:06 -0500

    add initial tooltip structure

---
+++ src/components/Vis/Tooltip.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { xlink_attr } from 'svelte/internal';
+
+	export let tooltipPos: [number, number] | [null, null];
+	$: [x, y] = tooltipPos;
+</script>
+
+<div class="tooltip" class:visible={x && y} style="transform:translate({x}px,{y}px);">
+	<slot>default slot</slot>
+</div>
+
+<style lang="scss">
+	.tooltip {
+		position: absolute;
+		transition: transform 50ms, opacity 100ms;
+		width: 10em;
+		min-height: 10px;
+		background: var(--text-color-grey);
+		color: var(--background-color);
+		pointer-events: none;
+		margin: 1em;
+		padding: 0.5em;
+		border-radius: 5px;
+		box-shadow: var(--shadow-elevation-medium);
+
+		&:not(.visible) {
+			opacity: 0;
+		}
+	}
+</style>
--- src/components/Vis/Vis.svelte
+++ src/components/Vis/Vis.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import * as d3 from 'd3';
 
-	import type { AnnotationType, SleepLog } from '$src/data/types';
+	import type { SleepLog } from '$src/data/types';
 	import { visMode } from '$src/store';
 
 	import rawData from '$src/data/sleepLogs.csv?raw';
@@ -9,10 +9,13 @@
 	import { COLOR_PALATTE, FORMATTERS, KEYS, MODES } from '$src/utils/constants';
 	import YAxis from './YAxis.svelte';
 	import Annotation from './Annotation.svelte';
+	import Tooltip from './Tooltip.svelte';
 
 	let width;
 	let height;
 	let padding = { top: 50, bottom: 150, left: 40, right: 50 };
+	let tooltipPos: [number, number] | [null, null] = [null, null];
+	let tooltipContent;
 
 	// DATA + TRANSFORMATIONS
 	let parsedData = d3
@@ -76,6 +79,14 @@
 		} else return `translate(${xScale(date)}px, 0)`;
 	};
 
+	const handleMouseover = (e: MouseEvent, log: SleepLog) => {
+		tooltipPos = [e.offsetX, e.offsetY];
+		tooltipContent = `${FORMATTERS.date(log.Start)}`;
+	};
+	const handleMouseout = (e: MouseEvent, log: SleepLog) => {
+		tooltipPos = [null, null];
+	};
+
 	// only include annotations that exist in domain
 	$: annotations = rawAnnotations.filter((a) => xScale.domain().includes(a.date));
 </script>
@@ -110,6 +121,8 @@
 						<rect
 							fill={colorScale(log.timeToEnd - log.timeToStart)}
 							rx="3"
+							on:mouseenter={(e) => handleMouseover(e, log)}
+							on:mouseleave={(e) => handleMouseout(e, log)}
 							style="q
 							filter:url(#glow);
 							transition-delay: {i * 5}ms;
@@ -123,6 +136,9 @@
 			{/each}
 		</g>
 	</svg>
+	<Tooltip {tooltipPos}>
+		<div>{tooltipContent}</div>
+	</Tooltip>
 </div>
 
 <style lang="scss">
@@ -141,11 +157,15 @@
 	}
 
 	.bars {
-		pointer-events: none;
+		cursor: pointer;
 		rect,
 		g {
 			transition: transform var(--fast-transition-duration), width var(--slow-transition-duration),
 				height var(--slow-transition-duration);
+		}
+
+		rect:hover {
+			stroke: blanchedalmond;
 		}
 	}
 </style>
